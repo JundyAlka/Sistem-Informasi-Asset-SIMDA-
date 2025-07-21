ec10038d57cc0d664d8fb48c157d088d12cdc879
JundyAlka <jundy.alkr@gmail.com>
2025-07-21 09:47:04 +0700

    Update config.php

--- sim_aset/application/config/config.php
+++ sim_aset/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sistem-informasi-asset-simda.vercel.app/';
+$config['base_url'] = 'http://localhost/simda-revisi-final/sim_aset/';
 /*
 |--------------------------------------------------------------------------
 | Index File
